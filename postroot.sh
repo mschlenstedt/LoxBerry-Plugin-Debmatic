cc2ebee70a9e5a08e9db97ee8d69ca7f058437ae
Michael Schlenstedt <Michael@schlenn.net>
2024-04-03 07:17:36 +0200

    Bugfix

--- postroot.sh
+++ postroot.sh
@@ -212,7 +212,7 @@ jq ".MQTT.Bridge.Password = \"$brokerpass\"" /etc/config/addons/ccu-jack.cfg > /
 mv /tmp/ccu-jack.cfg /etc/config/addons/ccu-jack.cfg
 jq ".MQTT.Bridge.ClientID = \"$hostname\"" /etc/config/addons/ccu-jack.cfg > /tmp/ccu-jack.cfg 
 mv /tmp/ccu-jack.cfg /etc/config/addons/ccu-jack.cfg
-if [ UPGRADE -eq 0 ]; then
+if [ UPGRADE = 0 ]; then
 	jq '.Users.loxberry = {
 		"Identifier": "loxberry",
 		"Active": true,
